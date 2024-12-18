d98f33cf64ec825c2776548d064c33a7970b0920
Bohdan <Bohdan.Bochkovskyi@gmail.com>
2024-12-18 14:30:18 -0600

    Add LanguageFlag formula

--- languageflag.rb
+++ languageflag.rb
@@ -1,20 +1,12 @@
-class Languageflag < Formula
-  desc "LanguageFlag - Show keyboard layouts with flags"
-  homepage "https://github.com/swiftyuser/LanguageFlag"
-  url "https://github.com/swiftyuser/LanguageFlag/releases/download/1.0.0/languageflag.zip"
+cask "languageflag" do
+  version "1.0.0"
   sha256 "a16b997216596ea973c9ee81bb2a9e24a751a3c6d91a0a5966e76040db164c12"
-  license "MIT"
 
-  # Dependencies (if applicable)
-  # depends_on "some_dependency"
+  url "https://github.com/swiftyuser/LanguageFlag/releases/download/1.0.0/languageflag.zip"
+  name "LanguageFlag"
+  desc "Show keyboard layouts with flags"
+  homepage "https://github.com/swiftyuser/LanguageFlag"
 
-  def install
-    # Install binary to Homebrew's bin directory
-    bin.install "languageflag"
-  end
-
-#  test do
-#    # Test your app's functionality (optional, but recommended)
-#    assert_match "LanguageFlag v1.0.0", shell_output("#{bin}/languageflag --version")
-#  end
+  # Move the `.app` to `/Applications`
+  app "LanguageFlag.app"
 end
